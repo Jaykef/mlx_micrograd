4ddfdb0e5e0527e008acc47bba9e2cb5d457b02d
Jaward Sesay <jkef96@icloud.com>
2024-05-05 19:17:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools, find_packages
+import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Jaykef/mlx_micrograd",
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
